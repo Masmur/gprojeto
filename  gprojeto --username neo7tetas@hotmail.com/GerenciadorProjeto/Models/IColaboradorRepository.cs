50f17f45cabf53d78b248f49bfdae7f730da10df
neo7tetas <neo7tetas@hotmail.com>
2010-03-10 20:24:08 +0000

    -- Atualização diaria

---
+++  gprojeto --username neo7tetas@hotmail.com/GerenciadorProjeto/Models/IColaboradorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GerenciadorProjeto.Models
+{
+    public interface IColaboradorRepository
+    {
+        IQueryable<Colaborador> GetAllColaboradores(int EmpresaId);
+        void DeleteAColaborador(int EmpresaId);
+        void AddColaborador(Colaborador NewColaborador);
+        void EditColaborador(Colaborador ColaboradorEdited);
+        Colaborador GetColaboradorById(int ColaboradorId);
+    }
+}
